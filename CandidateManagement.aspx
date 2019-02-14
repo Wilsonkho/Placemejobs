1ce936f1e4ac477bfac9344565ab88ce86d01e8f
PatrickChan168 <31664171+PatrickChan168@users.noreply.github.com>
2019-02-14 14:03:11 -0700

    Candidate Management

---
+++ CandidateManagement.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Candidate Management" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="CandidateManagement.aspx.cs" Inherits="CandidateManagement" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <asp:Table ID="Results" runat="server" border="1"/>
+</asp:Content>
+
---
+++ CandidateManagement.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CandidateManagement : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TableHeaderRow ResultsTHRow = new TableHeaderRow();
+
+        List<String> HeaderList = new List<String>()
+        {
+            "Header1","Header2","Header3"
+        };
+
+        foreach (string header in HeaderList)
+        {
+            TableHeaderCell ResultsTHCell = new TableHeaderCell();
+            ResultsTHCell.Text = header;
+
+            ResultsTHRow.Cells.Add(ResultsTHCell);
+        }
+
+        Results.Rows.Add(ResultsTHRow);
+    }
+}
