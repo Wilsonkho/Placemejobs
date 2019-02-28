5071b8b68c8c35593f8c7f72388814da4e545325
PatrickChan168 <31664171+PatrickChan168@users.noreply.github.com>
2019-02-28 16:13:04 -0700

    View uploaded pdf

--- CandidateManagement.aspx.cs
+++ CandidateManagement.aspx.cs
@@ -11,4 +11,16 @@ public partial class CandidateManagement : System.Web.UI.Page
     {
 
     }
+
+    //protected void ViewButton1_Click(object sender, EventArgs e)
+    //{
+    //    User currentUser = new User();
+    //    PRMS controller = new PRMS();
+
+    //    currentUser = controller.GetUserCVByEmail("newguy@email.com");
+    //    string path = Server.MapPath("~/Files/" + currentUser.UserID + "/" + "Resume/" + currentUser.Resume);
+
+    //    System.Diagnostics.Process.Start("Firefox.exe", path);
+    //}
+
 }
