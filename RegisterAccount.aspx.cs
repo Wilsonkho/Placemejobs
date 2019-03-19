d226eb1a193a1c7893690626ad0bd2b046f85d1c
PatrickChan168 <31664171+PatrickChan168@users.noreply.github.com>
2019-03-19 15:00:44 -0600

    itextsharp, registeraccount front end

---
+++ RegisterAccount.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class RegisterAccount : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Submit_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Clear_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void AddRegion_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void AddSkill_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void AddProfession_Click(object sender, EventArgs e)
+    {
+
+    }
+}
