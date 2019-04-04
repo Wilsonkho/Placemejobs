508b2fc2ddcdf964f272ec157afca7fab2f97f23
PatrickChan168 <31664171+PatrickChan168@users.noreply.github.com>
2019-04-04 13:58:30 -0600

    Add donotreply email, list job postings

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,7 +11,47 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        PRMS controller = new PRMS();
+        // Get all job postings
+        List<JobPosting> jobPostingsList = new List<JobPosting>();
+        jobPostingsList = controller.GetAllJobPostings();
 
+        // Table Headings
+        TableHeaderRow tableHRow = new TableHeaderRow();
+        List<String> headerList = new List<String>()
+        {
+            "Company Name", "Job Description"
+        };
+
+        foreach (string header in headerList)
+        {
+            TableHeaderCell tableHCell = new TableHeaderCell();
+            tableHCell.Text = header;
+
+            tableHRow.Cells.Add(tableHCell);
+        }
+
+        JobPostingsTable.Rows.Add(tableHRow);
+
+        // Table Rows
+        TableRow aNewRow;
+        int index = 0;
+
+        foreach (var item in jobPostingsList)
+        {
+            aNewRow = new TableRow();
+
+            TableCell aNewCell = new TableCell();
+            aNewCell.Text = item.CompanyName;
+            aNewRow.Cells.Add(aNewCell);
+
+            aNewCell = new TableCell();
+            aNewCell.Text = item.Description;
+            aNewRow.Cells.Add(aNewCell);
+
+            JobPostingsTable.Rows.Add(aNewRow);
+            index++;
+        }
 
     }
 
