b68faf6b9165d5e102c24e57cec0931c3e880d24
PatrickChan168 <31664171+PatrickChan168@users.noreply.github.com>
2019-04-16 14:12:35 -0600

    Changed Email

--- RegisterAccount.aspx.cs
+++ RegisterAccount.aspx.cs
@@ -202,7 +202,7 @@ public partial class RegisterAccount : System.Web.UI.Page
                         string email = EmailTextBox.Text;
                         string firstName = FirstName.Text;
 
-                        MailAddress from = new MailAddress("donotreplytoplacemejob@gmail.com");
+                        MailAddress from = new MailAddress("automatedreplyfromplacemejob@gmail.com");
                         MailAddress to = new MailAddress(email);
                         MailMessage message = new MailMessage(from, to);
 
@@ -218,7 +218,7 @@ public partial class RegisterAccount : System.Web.UI.Page
                         SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
                         client.UseDefaultCredentials = false;
                         client.EnableSsl = true;
-                        client.Credentials = new NetworkCredential("donotreplytoplacemejob@gmail.com", "placeme123");
+                        client.Credentials = new NetworkCredential("automatedreplyfromplacemejob@gmail.com", "mejob986");
 
                         try
                         {
@@ -325,4 +325,32 @@ public partial class RegisterAccount : System.Web.UI.Page
 
         Session["professions"] = professionList;
     }
+
+    //protected void SendMailBASIC()
+    //{
+    //    // Gmail Address from where you send the mail
+    //    var fromAddress = "no.reply.company@gmail.com";
+    //    // any address where the email will be sending
+    //    var toAddress = Email.Text.ToString();
+    //    //Password of your gmail address
+    //    const string fromPassword = "password";
+    //    // Passing the values and make a email formate to display
+    //    string subject = "Customer Account Confirmation";
+    //    string body = "Hi " + Name.Text.ToString() + "\n\n";
+    //    body += "Welcome to blah\nI Wanted to let you know that your account for blah.com is now active. " + "\n\n";
+    //    body += "The next time you log in we will ask you to add more details to your account.  " + "\n";
+    //    body += "\nCheers, \n\nThe Baist team";
+    //    // smtp settings
+    //    var smtp = new System.Net.Mail.SmtpClient();
+    //    {
+    //        smtp.Host = "smtp.gmail.com";
+    //        smtp.Port = 587;
+    //        smtp.EnableSsl = true;
+    //        smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+    //        smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
+    //        smtp.Timeout = 20000;
+    //    }
+    //    // Passing values to smtp object
+    //    smtp.Send(fromAddress, toAddress, subject, body);
+    //}
 }
